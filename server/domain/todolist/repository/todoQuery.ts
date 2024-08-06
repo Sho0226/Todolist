b7c14270bf62548d3c3972804f4ace54ff9cfbc0
Sho0226 <s1F102201616@iniad.org>
2024-08-07 04:25:13 +0900

    fix: id

--- server/domain/todolist/repository/todoQuery.ts
+++ server/domain/todolist/repository/todoQuery.ts
@@ -1,10 +1,22 @@
 import { Prisma, type Todo } from '@prisma/client';
+import { number, string } from 'zod';
 
 
 const getAllTodos = async (tx: Prisma.TransactionClient): Promise<Todo[]> => {
   return await tx.todo.findMany()
 };
 
-const createTodo =
+const createTodo = async (
+  tx: Prisma.TransactionClient,
+  title: string,
+): Promise<Todo > => {
+  return await tx.todo.create({
+    data: {
+      title,
+      createdAt: new Date(),
+      updateAt: new Date()
+    },
+  });
+};
 
 const updateTodo =
