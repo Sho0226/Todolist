cbc4458155ba49fde31a89c1e01fe7531dcc976a
Sho0226 <s1F102201616@iniad.org>
2024-08-08 15:40:43 +0900

    fix:usecasw

--- server/domain/todolist/useCase/todoUseCase.ts
+++ server/domain/todolist/useCase/todoUseCase.ts
@@ -11,19 +11,19 @@ export const todoUseCase = {
 
   createTodo: async (title: string): Promise<Todo> =>
     transaction('RepeatableRead', async (tx) => {
-      const create = await todoQuery.createTodo(tx, title);
-      return create;
+      const createTodo = await todoQuery.createTodo(tx, title);
+      return createTodo;
     }),
 
   updateTodo: async (id: number, title: string): Promise<Todo | null> =>
     transaction('RepeatableRead', async (tx) => {
-      const update = await todoQuery.updateTodo(tx, id, title);
-      return update;
+      const updateTodo = await todoQuery.updateTodo(tx, id, title);
+      return updateTodo;
     }),
 
   deleteTodo: async (id: number): Promise<boolean> =>
     transaction('RepeatableRead', async (tx) => {
-      const deletes = await todoQuery.deleteTodo(tx, id);
-      return deletes;
+      const deleted = await todoQuery.deleteTodo(tx, id);
+      return deleted;
     }),
 };
