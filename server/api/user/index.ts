f95b59e21691ef64bcfd994bd4d4df2175d42b48
Sho0226 <s1F102201616@iniad.org>
2024-10-08 03:08:07 +0900

    add: user

--- server/api/user/index.ts
+++ server/api/user/index.ts
@@ -1,7 +1,11 @@
 import type { DefineMethods } from 'aspida';
 
 export type Methods = DefineMethods<{
-  get: {
-    resBody: string;
+  post: {
+    reqBody: {
+      name: string;
+      password: string;
+    };
+    resBody: { token: string };
   };
 }>;
